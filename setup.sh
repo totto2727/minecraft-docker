9f3d5968086f0cc9c40826d3fea0dc8e5856dc44
totto2727 <kaihatu.totto2727@gmail.com>
2022-01-15 05:58:58 +0000

    fix

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
 sudo groupadd docker
-usermod -aG docker $USER
+sudo usermod -aG docker $USER
 (crontab -l; echo "@reboot cd ${PWD} && docker compose up -d") | crontab -
