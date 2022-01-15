45ccbef3ed4bf2edbd6233f8dfd8c111450f6dba
totto2727 <kaihatu.totto2727@gmail.com>
2022-01-15 05:51:22 +0000

    script追加

---
+++ README.md
@@ -0,0 +1,5 @@
+# 使い方
+Amazon Linux 2での利用を想定としています。
+1. `setup.sh`を管理者権限で実行
+2. `.env.template`ファイルに従って`.env`ファイルを作成
+2. `docker compose up -d`を実行
---
+++ setup.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+amazon-linux-extras install docker
+systemctl enable docker
+systemctl start docker
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+groupadd docker
+usermod -aG docker $USER
+(crontab -l; echo "@reboot cd ${PWD} && docker compose up -d") | crontab -
