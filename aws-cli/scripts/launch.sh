b720c73fb045e696ee9b5bea35b1a1cbbf18cbc5
totto2727 <kaihatu.totto2727@gmail.com>
2022-01-15 04:52:28 +0000

    nginx追加

--- aws-cli/scripts/launch.sh
+++ aws-cli/scripts/launch.sh
@@ -3,6 +3,6 @@
 cd /opt/minecraft-server
 
 NOW=`date "+%Y%m%d"`
-zip ${NOW}.zip -r world world_nether world_the_end/
-aws s3 cp ${NOW}.zip s3://${S3_BUCKET_NAME}/${MINECRAFT_SERVER_NAME}/world_backup/ --storage-class ONEZONE_IA
-rm -f ${NOW}.zip
+zip ~/${NOW}.zip -r world world_nether world_the_end/
+aws s3 cp ~/${NOW}.zip s3://${S3_BUCKET_NAME}/${MINECRAFT_SERVER_NAME}/world_backup/ --storage-class ONEZONE_IA
+rm -f ~/${NOW}.zip
