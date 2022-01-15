6d56bdeb337728302de13255716b6075055937b3
totto2727 <kaihatu.totto2727@gmail.com>
2022-01-15 05:54:47 +0000

    fix

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 
-amazon-linux-extras install docker -y
-systemctl enable docker
-systemctl start docker
+sudo amazon-linux-extras install docker -y
+sudo systemctl enable docker
+sudo systemctl start docker
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
-groupadd docker
+sudo groupadd docker
 usermod -aG docker $USER
 (crontab -l; echo "@reboot cd ${PWD} && docker compose up -d") | crontab -
